9c643825bbc8e01ed7e76f6be33cd9530275b544
liviabbxavier <liviabrocardo43@gmail.com>
2026-04-17 11:44:38 -0300

    Add files via upload

---
+++ Produto/Produto.cs
@@ -0,0 +1,18 @@
+class Produto
+{
+    public string nome;
+    public string descricao;
+    public decimal preco;
+    public int estoque;
+
+    public bool EstaDisponivel()
+    {
+        return estoque > 0;
+    }
+
+    public void AlteraPrecoComDesconto(decimal desconto)
+    {
+        preco = preco * (1 - desconto);
+    }
+
+}
